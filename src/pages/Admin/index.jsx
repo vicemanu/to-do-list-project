8dbf74aad5c74ba6c5cc6b48745d29fcb3d2a60f
Victor Mielke <vicemanu31@outlook.com>
2023-04-14 19:55:46 -0300

    criando o sistema de edit

--- src/pages/Admin/index.jsx
+++ src/pages/Admin/index.jsx
@@ -4,17 +4,41 @@ import './admin.css'
 import { auth, data } from '../../firebase'
 import { signOut } from 'firebase/auth'
 
-import { addDoc, collection } from 'firebase/firestore'
+import { addDoc, collection, onSnapshot, query, orderBy, where, doc, deleteDoc, updateDoc } from 'firebase/firestore'
 
 export default function Admin() {
 
-    const [tarefa, setTarefa] = useState('')
-    const [user, setUser] = useState({})
+    const [tarefa, setTarefa] = useState('');
+    const [user, setUser] = useState({});
+    const [listaTarefas, setListaTarefas] = useState([]);
+    const [edit, setEdit] = useState([]);
 
 useEffect(()=> {
     async function loadTarefas() {
         const userDetail = localStorage.getItem("@detailUser")
         setUser(JSON.parse(userDetail))
+
+        if(userDetail) {
+            const dados = JSON.parse(userDetail)
+
+            const tarefaRef = collection(data, "tarefas")
+            const q = query(tarefaRef, orderBy("created", "desc"), where("userUid", "==", dados?.uid))
+            const unsub = onSnapshot(q, (snapshot)=> {
+                    let lista = [];
+
+                    snapshot.forEach((doc)=> {
+                        lista.push ({
+                            id: doc.id,
+                            tarefa: doc.data().tarefa,
+                            userUid: doc.data().userUid
+                        })
+                    })
+                    setListaTarefas(lista);
+            })
+        }
+
+
+
     }
     loadTarefas()
 },[])
@@ -26,6 +50,12 @@ useEffect(()=> {
 
         if(tarefa === '') {
             alert("Digite sua tarefa...")
+            return;
+        }
+
+
+        if(edit?.id) {
+            handleUpdateTarefa();
             return;
         }
 
@@ -47,6 +77,34 @@ useEffect(()=> {
         await signOut(auth);
     }
 
+    async function deleteTarefa(id) {
+        const docRef = doc(data, "tarefas", id)
+        await deleteDoc(docRef)
+    }
+
+    function editTarefa(item) {
+        setTarefa(item.tarefa);
+        setEdit(item);
+    }
+
+
+    async function handleUpdateTarefa() {
+        const docRef = doc(data, "tarefas", edit?.id)
+        await updateDoc(docRef, {
+            tarefa: tarefa
+        })
+        .then(()=> {
+            console.log("tarefa atualizada")
+            setTarefa('')
+            setEdit({})
+        })
+        .catch(()=> {
+            console.log("erro ao atualizar")
+            setTarefa('')
+            setEdit({})
+        })
+    }
+
     return(
     <div className='admin'>
         <h1>Minhas Tarefas</h1>
@@ -56,18 +114,27 @@ useEffect(()=> {
             value={tarefa}
             onChange={e => {setTarefa(e.target.value)}}
             />
-
-            <button className='btn-register' type='submit'> Registrar tarefa</button>
+                {Object.keys(edit).length > 0 ? (
+                    <button className='btn-register' style={{backgroundColor: '#5ab134'}} type='submit'> Atualizar tarefas</button>
+                ):(
+                    <button className='btn-register' type='submit'> Registrar tarefa</button>
+                ) }
+            
         </form>
 
-        <article className='list'>
-            <p>Estudar Javascript</p>
+        {listaTarefas.map((item)=> {
+            return(
+            <article key={item.id} className='list'>
+                <p>{item.tarefa}</p>
+                <div>
+                    <button onClick={()=> editTarefa(item)}>Editar</button>
+                    <button onClick={()=> deleteTarefa(item.id)} className='btn-delete'>Concluir</button>
+                </div>
+            </article>
+            )
+        })}
 
-            <div>
-                <button>Editar</button>
-                <button className='btn-delete'>Concluir</button>
-            </div>
-        </article>
+       
 
         <button className='btn-logout' onClick={headleLogout}>Sair</button>
     </div>
